3b0942de2bb67e7932617893949067648e7ba918
Michał Mazurek <michal.mazurek@cern.ch>
2024-04-15 10:45:11 +0200

    Run formatting :broom: (#7)
    * Run formatting
    
    * Fix workflows

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from datetime import datetime
 try:
     import config as cfg
 except ImportError:
+
     class cfg(object):
         slots_to_check = StatusChecker.slots_to_check
         platforms_to_check = StatusChecker.platforms_to_check
@@ -136,28 +137,36 @@ platforms_to_check = [
         project_names=projects,
     )
     slots_list = [sn for sn in checker._slots.keys()]
-    miss_slots = [sn for sn in StatusChecker.slots_to_check
-                  if sn not in slots_list]
+    miss_slots = [
+        sn for sn in StatusChecker.slots_to_check if sn not in slots_list
+    ]
     if len(miss_slots) > 0:
-        logging.warning("Hardcoded default slots {} not found in Nightly page."
-                        " Maybe update package!".format(', '.join(miss_slots)))
+        logging.warning(
+            "Hardcoded default slots {} not found in Nightly page."
+            " Maybe update package!".format(", ".join(miss_slots))
+        )
     for slot, build in checker._slots.items():
         r = checker._get_Platforms_Projects_for_slot(slot, build)
         project_list += [pn for pn in r[1] if pn not in project_list]
         platform_list += [pn for pn in r[0] if pn not in platform_list]
-    slots_str = pretty_sep.join(['"{}"'.format(ss)
-                                 for ss in slots_list])
-    projects_str = pretty_sep.join(['"{}"'.format(ss)
-                                    for ss in project_list])
-    platforms_str = pretty_sep.join(['"{}"'.format(ss)
-                                     for ss in platform_list])
-    with open("config.py", 'w', encoding='utf-8') as fp:
-        fp.write(cfg_code.format(slots_list=slots_str,
-                                 project_list=projects_str,
-                                 platform_list=platforms_str))
+    slots_str = pretty_sep.join(['"{}"'.format(ss) for ss in slots_list])
+    projects_str = pretty_sep.join(['"{}"'.format(ss) for ss in project_list])
+    platforms_str = pretty_sep.join(
+        ['"{}"'.format(ss) for ss in platform_list]
+    )
+    with open("config.py", "w", encoding="utf-8") as fp:
+        fp.write(
+            cfg_code.format(
+                slots_list=slots_str,
+                project_list=projects_str,
+                platform_list=platforms_str,
+            )
+        )
         fp.flush()
-    logging.info("'config.py' file written to disk. "
-                 "Edit accordingly and run script again for desired function.")
+    logging.info(
+        "'config.py' file written to disk. "
+        "Edit accordingly and run script again for desired function."
+    )
 
 
 cli.add_command(current_status)
